aff09a41ca52d62cb9d4b53aedef9b7a16f432ae
boyang <yang.wilby@gmail.com>
2019-03-31 13:03:40 +0200

    make data kind of dynamic from network

--- lib/player/player.dart
+++ lib/player/player.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:audioplayer/audioplayer.dart';
 import 'package:http/http.dart' as http;
+
 class Player extends StatefulWidget {
   Player({Key key, this.title}) : super(key: key);
 
@@ -20,25 +21,26 @@ class Player extends StatefulWidget {
   @override
   _PlayerState createState() => _PlayerState();
 }
+
 class Music {
   final int userId;
   final int id;
-  final String title;
-  final String body;
+  final String name;
+  final String description;
 
-  Music({this.userId, this.id, this.title, this.body});
+  Music({this.userId, this.id, this.name, this.description});
 
   factory Music.fromJson(Map<String, dynamic> json) {
     return Music(
       userId: json['userId'],
       id: json['id'],
-      title: json['title'],
-      body: json['body'],
+      name: json['title'],
+      description: json['body'],
     );
   }
 }
-class _PlayerState extends State<Player> {
 
+class _PlayerState extends State<Player> {
   void _playAudio() {
     var audioPlayer = AudioPlayer();
     audioPlayer.play("https://sample-videos.com/audio/mp3/crowd-cheering.mp3");
@@ -46,7 +48,7 @@ class _PlayerState extends State<Player> {
 
   Future<Music> fetchMusic() async {
     final response =
-    await http.get('https://jsonplaceholder.typicode.com/posts/1');
+        await http.get('https://jsonplaceholder.typicode.com/posts/1');
 
     if (response.statusCode == 200) {
       // If server returns an OK response, parse the JSON
@@ -57,43 +59,6 @@ class _PlayerState extends State<Player> {
     }
   }
 
-  Widget titleSection = Container(
-    padding: const EdgeInsets.all(32),
-    child: Row(
-      children: [
-        Expanded(
-          /*1*/
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              /*2*/
-              Container(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: Text(
-                  'Oeschinen Lake Campground',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Text(
-                'Kandersteg, Switzerland',
-                style: TextStyle(
-                  color: Colors.grey[500],
-                ),
-              ),
-            ],
-          ),
-        ),
-        /*3*/
-        Icon(
-          Icons.star,
-          color: Colors.red[500],
-        ),
-        Text('41'),
-      ],
-    ),
-  );
   Column _buildButtonColumn(Color color, IconData icon, String label) {
     return Column(
       mainAxisSize: MainAxisSize.min,
@@ -114,6 +79,7 @@ class _PlayerState extends State<Player> {
       ],
     );
   }
+
   @override
   Widget build(BuildContext context) {
     Color color = Theme.of(context).primaryColor;
@@ -129,24 +95,6 @@ class _PlayerState extends State<Player> {
       ),
     );
 
-    Widget textSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-            'Alps. Situated 1,578 meters above sea level, it is one of the '
-            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-            'half-hour walk through pastures and pine forest, leads you to the '
-            'lake, which warms to 20 degrees Celsius in the summer. Activities '
-            'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true,
-      ),
-    );
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -154,49 +102,84 @@ class _PlayerState extends State<Player> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: ListView(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          children: <Widget>[
-            FutureBuilder<Music>(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: FutureBuilder(
               future: fetchMusic(),
               builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return Text(snapshot.data.title);
-                } else if (snapshot.hasError) {
-                  return Text("${snapshot.error}");
-                }
-
-                // By default, show a loading spinner
-                return CircularProgressIndicator();
-              },
-            ),
-            Image.network("https://images.unsplash.com/photo-1471115853179-bb1d604434e0?dpr=1&auto=format&fit=crop&w=767&h=583&q=80&cs=tinysrgb&crop=", width: 600, height: 240, fit: BoxFit.cover),
-            titleSection,
-            buttonSection,
-            textSection,
-          ],
-        ),
-      ),
+            
+            return ListView(
+              children: <Widget>[
+                Image.network(
+                    "https://images.unsplash.com/photo-1471115853179-bb1d604434e0?dpr=1&auto=format&fit=crop&w=767&h=583&q=80&cs=tinysrgb&crop=",
+                    width: 600,
+                    height: 240,
+                    fit: BoxFit.cover),
+                _buildTitleSection(snapshot.data),
+                buttonSection,
+                _buildDescSection(snapshot.data),
+              ],
+            );
+          })),
       floatingActionButton: FloatingActionButton(
         onPressed: _playAudio,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget _buildDescSection(Music music) {
+    String description = music.description;
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        '$description',
+        softWrap: true,
+      ),
+    );
+    return textSection;
+  }
+
+  Widget _buildTitleSection(Music music) {
+    String name = music.name;
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    '$name',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          Text('41'),
+        ],
+      ),
+    );
+    return titleSection;
   }
 }
